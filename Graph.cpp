52432695eb4f7646d38d79ac17d1df60b31994c1
MaciejZbierowski <maix19bep@gmail.com>
2016-12-29 00:50:49 +0100

    added comments

--- Graph.cpp
+++ Graph.cpp
@@ -80,13 +80,21 @@ void showPostOrder(int n, int *po){
 }
 
 int Graph::znajdzSasiada(int w){
+	
 	int l = 0;
+	
+	//petla znajduje sasiada, czyli wierzcholek do ktorego moze isc przeszukiwanie
+	//warunki to: komorka tablica nie moze miec 0 (tam gdzie jest krawedz jest 1) - czyli jak komorka ma 1ke
+	//wierzcholek nie moze byc wczesniej odwiedzony
+	//zawsze wybiera pierwzego spotkanego sasiada ktory spelnia dwa powyzsze warunki. Najmniejszego wzrostowo
 	for(int i = 0; i < size; i++){
 		if(graph[w][i] != 0 && !visited[i] && l < 1){
 			l++;
+			//przypisuje do parametru w nowego sasiada
 			w = i;
 		}
 	}
+	//i zwraca go
 	return w;
 }
 
@@ -97,10 +105,19 @@ bool Graph::sprawdzCzyMaSasiadow(int w){
 		if(graph[w][i] != 0 && !visited[i])
 			l++;
 	
+	//zlicza czy wierzcholek w ma sasiadow,czyli: czy jakies pozycje w tablicy z pierwzzym indeksem w maja drugi 
+	//rozny 0 (rowny 1) i czy te wierzcholki sa nieodwiedzone
+	
+	//jesli licznik jest wiekszy od 0 (tu rozny, mogloby byc wiekszy) to znaczy ze sa takowe wierzcholki ktore
+	//moze odwiedzic wierzcholek w.
 	return (l != 0) ? true : false;
 }
 
 void Graph::transposeMatrix(){
+	
+	//najpierw znajduje czy sa jakies wierzcholki dwukierunkowe czyli: np. 7-8 i jednoczesnie 8-7
+	//jesli takie sa to zmienia w dwie strony 1ki na 2ki. zrobilem to po to ze wystepowal potem konflikt
+	//on zamienial jedna strone a druga wymazywal.
 	
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++)
@@ -109,6 +126,8 @@ void Graph::transposeMatrix(){
 				graph[j][i] = 2;
 			}
 	
+	//teraz szuka 1-nek. tam gdzie znajdzie daje -1. i odwrotnej stronie daje 2ke. bo transpozycja ma odwrocic
+	//kierunki strzalek.
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++)
 			if(graph[i][j] == 1){
@@ -116,6 +135,7 @@ void Graph::transposeMatrix(){
 				graph[j][i] = 2;
 			}
 	
+	// i na koniec wymazuje -1.
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++)
 			if(graph[i][j] == -1){
@@ -136,12 +156,23 @@ void Graph::DFSPostOrder(int beg){
 
 	Stack stack(size);
 	
+	//wsadz pierwszy wierzcholek na stos
 	stack.push(beg);
+	//oznacz go jakos odwiedzony
 	visited[beg] = true;
 	
+	//dopoki stos nie jest pusty
 	while(!stack.checkIfStackEmpty()){
+		//wsadz na stos dostepnego sasiada wierzcholka ktory aktualnie jest na topie stosu
 		stack.push(znajdzSasiada(stack.getStackTop()));
+		//oznacz nowy top stosu jako odwiedzony
 		visited[stack.getStackTop()] = true;
+		//dopoki wierzcholek nie ma sasiadow. jesli wierzcholek ma sasiadow w ogole nie wchodzi do tej petli
+		//jesli nie ma to oznacza ten wierzcholek jako odwiedzony (tu wlasnie powstaje POST-ORDER. Idzie dopoki
+		//nie napotka wierzcholka z ktorego nigdzie nie mozna isc i wtedy dopiero ustala porzadek). Wrzuca ten
+		//wierzcholek do tablicy post-order i zdejmuje wierzcholek ze stosu dopoki nie natraf na taki wierzcholek
+		//ktory ma sasiadow jeszcze nie odwiedzonych. jesli w koncu natrafi na koniec stosu konczy dzialanie.
+		
 		while(!stack.checkIfStackEmpty() && !sprawdzCzyMaSasiadow(stack.getStackTop())){
 			visited[stack.getStackTop()] = true;
 			postorder[k] = stack.getStackTop();
@@ -149,27 +180,33 @@ void Graph::DFSPostOrder(int beg){
 			stack.pop();
 		}
 	}
-	
-	//showVisited(visited, size, postorder);
 		
 }
 
 void Graph::KosarajuAlgorithm(){
 	
+	//ten algorytm ma wykonywac na transponowanym grafie DFS-y. po kolei od konca tablicy POST-ORDER
+	
 	transposeMatrix();
 	
+	//tablica pomocnicza to przechowywania ustalonego wczesniej przez algorytm DFS POST ORDER porzadku.
 	int *x = new int[size];
 	
 	for(int i = 0; i < size; i++)
 		x[i] = postorder[i];
 	
 	cout<<"Zbiory wierzcholkow indukujace silnie spojne skladowe digrafu: "<<endl;
+	
 	int i = 1;
 	while(i <= size){
+		//jesli element tablicy x nie byl wczesniej ustawiony na -1
 		if(x[size-i] != -1){
+			//wykonaj DFS zaczynajac od ostatniego elementu z porzadku
 			DFSPostOrder(x[size-i]);
 			
 			cout<<"{ ";
+			//ta petla zaznacza w tablicy pomocniczej x ktorym wierzcholkom juz zrobilismy DFS. Po wykonaniu DFS() w
+			//tablicy visited mamy odwiedzone wierzcholki. Ustawia elementy tablicy x na -1
 			for(int i = 0; i < size; i++)
 				if(visited[i]){
 					for(int j = 0; j < size; j++){
